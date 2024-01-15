c41bd27599ab35a984f074afc5a743dab84bb0b8
ArunReddy26 <132929105+ArunReddy26@users.noreply.github.com>
2024-01-15 18:13:43 +0530

    Update CounterButton.js

--- components/CounterButton.js
+++ components/CounterButton.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-function CounterButton() {
+function CounterButton({ onIncrement }) {
      /*DO NOT EDIT THIS LINE */ console.log("CounterButton rendered!"); //DO NOT edit this line 
+      const handleClick = useCallback(() => {
+    console.log('Button clicked!');
+    onIncrement();
+  }, [onIncrement]);
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
 
 }
 
-export default /*___________*/
+export default React.memo(CounterButton);
