8b5109200594d189ed278c3a14e4365ee90bbeae
ArunReddy26 <132929105+ArunReddy26@users.noreply.github.com>
2024-01-15 19:04:55 +0530

    Update CounterButton.js

--- components/CounterButton.js
+++ components/CounterButton.js
@@ -3,7 +3,7 @@ import React, { useCallback } from 'react';
 function CounterButton({ onIncrement }) {
      /*DO NOT EDIT THIS LINE */ console.log("CounterButton rendered!"); //DO NOT edit this line 
       const handleClick = useCallback(() => {
-    console.log('Button clicked!');
+    // console.log('Button clicked!');
     onIncrement();
   }, [onIncrement]);
 
